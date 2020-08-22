f4ba8037318ebd7b96b7452a6b3b81928bb036ff
Maulik Sompura <maulik.sompura06@gmail.com>
2020-08-22 20:02:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: '4m8tm3j5VMdZCxi12J1YDJw57',
-    consumer_secret: 'xVLekHTf5094m7k0DpVhNiEEC0n0oE345UXDhYeutTJty5be5R',
-    // access_token_key: '219691400-KWeLQmw91spJKpnzWBIEEEQ59O2JK78Ia3wurceF', for npm twitter
-    access_token: '219691400-KWeLQmw91spJKpnzWBIEEEQ59O2JK78Ia3wurceF', // for npm twit
-    access_token_secret: 'D4dNkdCVgOcF9MhT7H2abSyudY4w3nQw4hyfemrO3P8OJ'
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    // access_token_key: process.env.access_token, for npm twitter
+    access_token: process.env.access_token, // for npm twit
+    access_token_secret: process.env.access_token_secret
   }
